65cf4f753a72e13e4aee2bb62f86550697ca87a2
CaptConrado <ottey001@gmail.com>
2014-02-04 21:41:19 -0800

    added some features resizign

--- app.rb
+++ app.rb
@@ -6,8 +6,38 @@ require 'raspell'
 # @speller = FFI::Aspell::Speller.new('en_US')
 @speller = Aspell.new("en_US")
 @speller.suggestion_mode = Aspell::NORMAL
-@image_size = 256
-image_url = "images/smalltext.png"
+# @image_size = 256
+image_url = ["images/art2.png"]
+
+
+
+image_url.each { |file|
+    puts file
+    img = Magick::Image::read(file).first
+    puts "   Format: #{img.format}"
+    puts "   Geometry: #{img.columns}x#{img.rows}"
+    puts "   Class: " + case img.class_type
+                            when Magick::DirectClass
+                                "DirectClass"
+                            when Magick::PseudoClass
+                                "PseudoClass"
+                        end
+    puts "   Depth: #{img.depth} bits-per-pixel"
+    puts "   Colors: #{img.number_colors}"
+    puts "   Filesize: #{img.filesize}"
+    puts "   Resolution: #{img.x_resolution.to_i}x#{img.y_resolution.to_i} "+
+        "pixels/#{img.units == Magick::PixelsPerInchResolution ?
+        "inch" : "centimeter"}"
+    if img.properties.length > 0
+        puts "   Properties:"
+        img.properties { |name,value|
+            puts %Q|      #{name} = "#{value}"|
+        }
+    end
+    }
+
+
+
 
 
 def tessrack(oimg_name, do_gray=true, keep_temp=true)
@@ -15,10 +45,14 @@ def tessrack(oimg_name, do_gray=true, keep_temp=true)
    
    # create a non-crooked version of the image
    tiff = Magick::Image::read(oimg_name).first.deskew 
+   tiff.scale!(2)
+   puts tiff.columns
+
+
 
    # convert to grayscale if do_gray==true
-   tiff = tiff.quantize(@image_size, Magick::GRAYColorspace) if do_gray == true
-   
+   tiff = tiff.quantize(256, Magick::GRAYColorspace) if do_gray == true
+
    # create a TIFF version of the file, as Tesseract only accepts TIFFs
    tname = "#{fname}--tesseracted.tif"
    tiff.write(tname){|t| t.depth = 16}
@@ -33,10 +67,7 @@ def tessrack(oimg_name, do_gray=true, keep_temp=true)
    File.open("#{fname}.txt"){|txt| txt.read}
 end
 
-txt = tessrack(image_url)
-
-
-
+txt = tessrack(image_url.first)
 
 def parse text
    text.split(' ')
@@ -56,6 +87,7 @@ end
 
 def word_replace text
    # text = "my haert wil go on"
+   puts text
    text.gsub(/[\w\']+/) do |word| 
      if !@speller.check(word) 
        # word is wrong
